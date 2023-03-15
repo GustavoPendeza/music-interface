16a16baaeb39957bba8aa984726af8f820c6da55
Gustavo S. Pendeza <g.pendeza@hotmail.com>
2023-03-15 18:26:06 -0300

    Add saudação baseada no horário

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,6 +7,8 @@ export function Home() {
     const { colorScheme, toggleColorScheme } = useColorScheme();
     const [isDark, setIsDark] = useState<boolean>(false);
     const data = require('../data/data.json');
+    const date = new Date();
+    const hour = date.getHours();
 
     useEffect(() => {
         colorScheme === "dark" ? setIsDark(true) : setIsDark(false);
@@ -14,9 +16,15 @@ export function Home() {
 
     return (
         <View className='flex-1 bg-white dark:bg-background pt-8'>
-            <ScrollView className='flex-1' showsVerticalScrollIndicator={false}>
 
-                <View className='flex-row justify-end items-center mx-5 mt-2'>
+            <View className='flex-row justify-between items-center mx-5 my-2'>
+                <View>
+                    <Text className='text-black dark:text-white text-lg font-nunitoBold'>
+                        {hour >= 6 ? hour >= 12 ? hour >= 19 ?  'Boa noite' : 'Boa tarde' : 'Bom dia' :'Boa madrugada'}
+                    </Text>
+                </View>
+
+                <View className='flex-row'>
                     <Text className='text-black dark:text-white text-base font-nunitoBold'>
                         {colorScheme === "dark" ? "DarkMode" : "LightMode"}
                     </Text>
@@ -27,6 +35,10 @@ export function Home() {
                         value={isDark}
                     />
                 </View>
+            </View>
+
+            <ScrollView className='flex-1' showsVerticalScrollIndicator={false}>
+
 
                 <List data={data} title="Gradient" route="gradient" />
 
